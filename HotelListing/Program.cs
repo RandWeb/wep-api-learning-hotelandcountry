6be54b100a2e3656b30b81e4948c802d87f7860f
OnlyGod <OnlyGod@DESKTOP-1VFTC4V>
2021-07-25 05:02:19 -0700

    Add project files.

---
+++ HotelListing/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(path: @"c:\hottellistings\logs\log-.txt",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.ff zz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger();
+
+            try 
+            {
+                Log.Information("Application Is Starting");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+
+                Log.Fatal(ex,"Application Failed To start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .UseStartup<Startup>();
+    }
+}
