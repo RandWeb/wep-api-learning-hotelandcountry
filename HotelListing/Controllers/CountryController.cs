61aa67c6cbde6ee19847698414c6c85eed2af1c9
OnlyGod <OnlyGod@DESKTOP-1VFTC4V>
2021-07-29 00:14:31 -0700

    actionMethod-GetAll

--- HotelListing/Controllers/CountryController.cs
+++ HotelListing/Controllers/CountryController.cs
@@ -15,7 +15,32 @@ namespace HotelListing.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
-      
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CountryController> _logger;
+        private readonly IMapper _mapper;
 
+        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper = null)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                var countries = await _unitOfWork.Countries.GetAll();
+                var results = _mapper.Map<IList<CountryDTO>>(countries);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Somthing Went Wrong in the {nameof(GetCountries)}");
+                return BadRequest(ex);
+                // return StatusCode(500,"Internal Server Error, Pease Try Again Later");
+            }
+        }
+       
     }
 }
